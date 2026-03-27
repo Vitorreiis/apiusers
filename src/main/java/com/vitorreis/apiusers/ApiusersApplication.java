1ddb643124b60bc34aeb7ae770e346048a5b2d65
Vitor Reis <vitorreis0303@gmail.com>
2026-03-26 21:51:07 -0300

    Criação e configuração do projeto.

---
+++ src/main/java/com/vitorreis/apiusers/ApiusersApplication.java
@@ -0,0 +1,13 @@
+package com.vitorreis.apiusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiusersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiusersApplication.class, args);
+	}
+
+}
